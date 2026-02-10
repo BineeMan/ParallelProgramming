84c6460a96d595ef343cabfb71ba2d02ac865975
George <e.plakhotin@g.nsu.ru>
2026-02-10 10:28:37 +0700

    patch

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -10,7 +10,7 @@
 class Timer {
 public:
     void Reset() {
-         clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     }
 
     Timer() {
@@ -20,7 +20,7 @@ public:
     double GetDurationSec() const {
         struct timespec end;
         clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-        return end.tv_sec-start.tv_sec + 0.000000001 * (end.tv_nsec - start.tv_nsec);
+        return end.tv_sec - start.tv_sec + 0.000000001 * (end.tv_nsec - start.tv_nsec);
     }
 
 private:
@@ -38,10 +38,10 @@ private:
     size_t Cols;
 
 public:
-    
     Matrix() = default;
 
     Matrix(const Matrix& other) = default;
+
     Matrix(Matrix&& other) = default;
 
     Matrix(size_t rows, size_t cols) {
@@ -63,7 +63,6 @@ public:
         Data = vector;
         Rows = rows;
         Cols = cols;
-
     }
 
     Matrix& operator=(const Matrix& other) {
@@ -98,7 +97,7 @@ public:
 
         for (size_t i = 0; i < GetRows(); i++) {
             for (size_t j = 0; j < other.GetCols(); j++) {
-                double sum { 0.0f };
+                double sum{ 0.0f };
                 for (size_t k = 0; k < GetCols(); k++) {
                     sum += this->At(i, k) * other.At(k, j);
                 }
@@ -199,12 +198,15 @@ public:
     }
 };
 
-template <typename T>
+template<typename T>
 void PrintVector(const std::vector<T>& vector) {
-    for (T element : vector) {
+    for (T element: vector) {
         std::cout << element << " ";
     }
     std::cout << std::endl;
+}
+
+void FillArray(const std::vector<double>& vector) {
 }
 
 std::vector<double> operator*(const std::vector<double>& A, const std::vector<double>& B) {
@@ -229,60 +231,90 @@ std::vector<double> operator-(const std::vector<double>& A, const std::vector<do
     return result;
 }
 
+std::vector<double> operator*(double scalar, const std::vector<double>& vector) {
+    std::vector<double> result(vector.size());
+    for (size_t i = 0; i < vector.size(); i++) {
+        result[i] = scalar * vector[i];
+    }
+    return result;
+}
+
+double Norm2(const std::vector<double>& vector) {
+    double sum{ 0.0f };
+    for (double element: vector) {
+        sum += element * element;
+    }
+    return std::sqrt(sum);
+}
+
+std::vector<double> SolveLinearEquation(const Matrix& A, const std::vector<double>& b, double epsilon) {
+    const size_t size = b.size();
+    if (!A.IsSquare() || b.size() != A.GetRows()) {
+        throw std::invalid_argument("!matrix.IsSquare() || rightPart.size() != matrix.GetRows()");
+    }
+    double tay{ 0.01 };
+    std::vector<double> x(size);
+
+    double normB = Norm2(b);
+
+    while ( Norm2(A * x - b) / normB >= epsilon ) {
+        x = x - tay * (A * x - b);
+    }
+    return x;
+}
+
+std::vector<double> SolveLinearEquation_Mpi1(const Matrix& A, const std::vector<double>& b,
+    double epsilon, int rank, int size) {
+    if (rank <= 0 || size <= 0) {
+        throw std::runtime_error("rank <= 0 || size <= 0");
+    }
+    if (!A.IsSquare() || b.size() != A.GetRows()) {
+        throw std::invalid_argument("!matrix.IsSquare() || rightPart.size() != matrix.GetRows()");
+    }
+
+
+    std::vector<double> x(b.size());
+    const double normB = Norm2(b);
+
+    std::vector<double> Ax = A * x;
+
+    const int base = A.GetRows() / size;
+    const int reminder = A.GetRows() % size;
+
+    const int rowsPerProcess = base + (rank < reminder ? 1 : 0);
+
+    const int startRow = rank * base + std::min()
+
+
+    while ( Norm2(Ax - b) / normB >= epsilon ) {
+        const double tay{ 0.01 };
+
+        x = x - tay * (Ax - b);
+        Ax = A * x;
+    }
+    return x;
+}
+
+
 int main(int argc, char** argv) {
-    // int N = 12000;
-    // Matrix matrix(N, N, 1.0);
-    // for (int i = 0; i < N; i++) {
-    //     matrix.At(i, i) = 2.0;
-    // }
-    //
-    // const double epsilon { std::pow(10, -5) };
-    //
-    // std::vector<double> b(N, N + 1);
-    // Timer timer;
-    // auto res = SolveLinearEquation(matrix, b, epsilon);
-    // std::cout << "Time: " << timer.GetDurationSec() << std::endl;
-    // PrintVector(res);
-
     MPI_Init(&argc, &argv);
-
     int rank, size;
-
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const int N = 16;
-
-    std::vector<int> data(N);
-    std::vector<int> localData(N / size);
+    //int N = 12000;
+    int N = 100;
+    Matrix matrix(N, N, 1.0);
     if (rank == 0) {
-        data.resize(N);
-        for (size_t i = 0; i < N; i++) {
-            data[i] = 1;
+        for (int i = 0; i < N; i++) {
+            matrix.At(i, i) = 2.0;
         }
-        std::cout << "Vector: ";
-        PrintVector(data);
     }
 
-    MPI_Scatter(data.data(), N / size, MPI_INT,
-        localData.data(), N / size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    std::cout << "Rank: " << rank << "; Vector: ";
-    PrintVector(localData);
-    std::cout << std::endl;
-
-    int sum = 0;
-    for (int x : localData) {
-        sum += x;
-    }
-
-    int globalSum = 0;
-
-    MPI_Reduce(&sum, &globalSum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    if (rank == 0)
-    std::cout << "Global Sum: " << globalSum << std::endl;
-
-    MPI_Finalize();
-
-    return 0;
+    const double epsilon{ std::pow(10, -5) };
+    std::vector<double> b(N, N + 1);
+    Timer timer;
+    auto res = SolveLinearEquation_Mpi1(matrix, b, epsilon, rank, size);
+    std::cout << "Time: " << timer.GetDurationSec() << std::endl;
+    PrintVector(res);
 }
