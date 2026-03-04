6a3c5eadebb81e46c69766e95d3ba82c9b99e122
Your Name <@example.com>
2026-03-04 16:03:06 +0700

    asd

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <thread>
 #include <vector>
+#include <omp.h>
 
 class Timer {
 public:
@@ -320,11 +321,10 @@ std::vector<double> SolveLinearEquation_Naive(const Matrix& matrixA,
     while (true) {
         // A * x
         for (size_t i = 0; i < matrixA.GetRows(); i++) {
-            double sum{ 0.0 };
+            vecAx[i] = 0;
             for (size_t j = 0; j < matrixA.GetCols(); j++) {
-                sum += matrixA.At(i, j) * vecX.at(j);
+                vecAx[i] += matrixA.At(i, j) * vecX.at(j);
             }
-            vecAx[i] = sum;
         }
 
         //Ax - b
@@ -351,7 +351,138 @@ std::vector<double> SolveLinearEquation_Naive(const Matrix& matrixA,
     return vecX;
 }
 
+std::vector<double> SolveLinearEquation_ParallelA(const Matrix& matrixA,
+                                              const std::vector<double>& vecB,
+                                              double epsilon, double tau) {
+    if (!matrixA.IsSquare() || vecB.size() != matrixA.GetRows()) {
+        throw std::invalid_argument("!matrix.IsSquare() || rightPart.size() != matrix.GetRows()");
+    }
+
+    const double kNormB = Norm2(vecB);
+    if (kNormB == 0) {
+        throw std::runtime_error("normB == 0");
+    }
+    const size_t kVecSize = vecB.size();
+    std::vector<double> vecAx(kVecSize, 0.0);
+    std::vector<double> vecX(kVecSize, 0.0);
+    std::vector<double> vecAxMinusB(kVecSize, 0.0);
+    const int kMaxIterations = 1000;
+    int iterationCount = 0;
+    while (true) {
+        // A * x
+        #pragma omp parallel for
+        for (size_t i = 0; i < matrixA.GetRows(); i++) {
+            vecAx[i] = 0;
+            for (size_t j = 0; j < matrixA.GetCols(); j++) {
+                vecAx[i] += matrixA.At(i, j) * vecX.at(j);
+            }
+        }
+
+        //Ax - b
+        double sum = 0.0;
+        #pragma omp parallel for reduction(+:sum)
+        for (size_t i = 0; i < vecAx.size(); i++) {
+            vecAxMinusB[i] = vecAx[i] - vecB[i];
+            sum += vecAxMinusB[i] * vecAxMinusB[i];
+        }
+
+        if (sqrt(sum) / kNormB < epsilon) {
+            break;
+        }
+        //std::cout << sqrt(sum) << std::endl;
+        #pragma omp parallel for
+        for (size_t i = 0; i < vecX.size(); i++) {
+            vecX[i] = vecX[i] - tau * vecAxMinusB[i];
+        }
+
+        if (iterationCount > kMaxIterations) {
+            throw std::runtime_error("iterationCount > kMaxIterations");
+        }
+        iterationCount++;
+    }
+    return vecX;
+}
+
+std::vector<double> SolveLinearEquation_ParallelB(const Matrix& matrixA,
+                                              const std::vector<double>& vecB,
+                                              double epsilon, double tau) {
+    if (!matrixA.IsSquare() || vecB.size() != matrixA.GetRows()) {
+        throw std::invalid_argument("!matrix.IsSquare() || rightPart.size() != matrix.GetRows()");
+    }
+
+    const double kNormB = Norm2(vecB);
+    if (kNormB == 0) {
+        throw std::runtime_error("normB == 0");
+    }
+    const size_t kVecSize = vecB.size();
+    std::vector<double> vecAx(kVecSize, 0.0);
+    std::vector<double> vecX(kVecSize, 0.0);
+    std::vector<double> vecAxMinusB(kVecSize, 0.0);
+    const int kMaxIterations = 1000;
+    int iterationCount = 0;
+    double sum = 0;
+    bool isFoundAnswer = false;
+    #pragma omp parallel
+    {
+        while (true) {
+            #pragma omp barrier
+            // A * x
+            #pragma omp for
+            for (size_t i = 0; i < matrixA.GetRows(); i++) {
+                vecAx[i] = 0;
+                for (size_t j = 0; j < matrixA.GetCols(); j++) {
+                    vecAx[i] += matrixA.At(i, j) * vecX.at(j);
+                }
+            }
+            #pragma omp barrier
+
+            //Ax - b
+            #pragma omp master
+            {
+                sum = 0;
+            }
+            #pragma omp barrier
+
+            #pragma omp for reduction(+:sum)
+            for (size_t i = 0; i < vecAx.size(); i++) {
+                vecAxMinusB[i] = vecAx[i] - vecB[i];
+                sum += vecAxMinusB[i] * vecAxMinusB[i];
+            }
+            #pragma omp barrier
+
+            #pragma omp master
+            {
+                if (sqrt(sum) / kNormB < epsilon) {
+                    isFoundAnswer = true;
+                    #pragma omp flush(isFoundAnswer)
+                }
+            }
+            #pragma omp barrier
+            if (isFoundAnswer) {
+                break;
+            }
+
+            #pragma omp for
+            for (size_t i = 0; i < vecX.size(); i++) {
+                vecX[i] = vecX[i] - tau * vecAxMinusB[i];
+            }
+
+            if (iterationCount > kMaxIterations) {
+                throw std::runtime_error("iterationCount > kMaxIterations");
+            }
+            #pragma omp barrier
+            #pragma omp master
+            {
+                iterationCount++;
+            }
+        }
+
+    }
+    return vecX;
+}
+
 int main(int argc, char** argv) {
+    omp_set_num_threads(4);
 #if 0
     int N = 0;
     double tau = 0.0;
@@ -359,7 +490,7 @@ int main(int argc, char** argv) {
     N = atoi(argv[1]);
 #endif
 #if 1
-    const int N = 8000;
+    const int N = 13000;
     const double tau = 0.0001;
 #endif
 
@@ -373,8 +504,26 @@ int main(int argc, char** argv) {
     Timer timer;
     std::vector<double> result;
     double duration = 0;
+
+#if 1
     timer.Reset();
     result = SolveLinearEquation_Naive(matrixA, vecB, epsilon, tau);
     duration = timer.GetDurationSec();
     PrintResult(std::cout, "Naive", duration, result, 1.0);
+#endif
+
+#if 1
+    timer.Reset();
+    result = SolveLinearEquation_ParallelA(matrixA, vecB, epsilon, tau);
+    duration = timer.GetDurationSec();
+    PrintResult(std::cout, "ParallelA", duration, result, 1.0);
+#endif
+
+#if 1
+    timer.Reset();
+    result = SolveLinearEquation_ParallelB(matrixA, vecB, epsilon, tau);
+    duration = timer.GetDurationSec();
+    PrintResult(std::cout, "ParallelB", duration, result, 1.0);
+#endif
+
 }
